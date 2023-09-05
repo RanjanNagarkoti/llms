33aceb89e31a88036489aa48cb04e4d2ce4d40e6
Ranjan Nagarkoti <ranjan.nagarkoti@bajratechnologies.com>
2023-09-05 17:45:36 +0545

    feat: additional field in user table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[username first_name last_name avatar])
+  end
 
   private
 

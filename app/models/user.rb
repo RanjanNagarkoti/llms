56c4f0a488980f982de52478f664b013ce3b6ab5
Ranjan Nagarkoti <ranjan.nagarkoti@bajratechnologies.com>
2023-09-06 14:27:43 +0545

    feat: user attributes validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,23 @@ class User < ApplicationRecord
 
   # Associations
   has_one_attached :avatar
+
+  # Validations
+  validates_associated :avatar
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :username,
+            uniqueness: {
+              message: lambda do |_object, data|
+                "#{data[:value]} is already taken."
+              end
+            }
+
+  validates :password, length: { minimum: 8 }, format: { with: /\A(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\-=\[\]{};':",.<>?])[A-Za-z0-9!@#$%^&*()_+\-=\[\]{};':",.<>?]+\z/,
+                                                         message: 'should include at least one number, one uppercase letter, and one special character' }
+
+  validates :avatar, presence: { message: 'is required!' }
 end

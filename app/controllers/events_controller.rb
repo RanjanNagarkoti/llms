1326763489fa9ec0cff26202a1551b3cf6c80258
Ranjan Nagarkoti <ranjan.nagarkoti@bajratechnologies.com>
2023-09-08 13:11:45 +0545

    feat: create event  from admin panel

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,14 +2,35 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show]
 
   def index
-    @pagy, @events = pagy(Event.all.order(created_at: :asc))
+    @pagy, @events = pagy(Event.all.order(created_at: :desc))
   end
 
   def show; end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.create(event_params)
+
+    if @event.save
+      respond_to do |format|
+        format.html { redirect_to events_path, notice: 'Record successfully created.' }
+      end
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :time, :duration, :status, :visibility, :location,
+                                  :type_id, :cover_picture, :content)
   end
 end

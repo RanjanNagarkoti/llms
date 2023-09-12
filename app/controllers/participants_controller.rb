7a126fd95d5913acc41cb8209945bf5f9c4d72cd
Ranjan Nagarkoti <ranjan.nagarkoti@bajratechnologies.com>
2023-09-12 14:41:13 +0545

    feat: send request after action performed

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -6,25 +6,16 @@ class ParticipantsController < ApplicationController
     @participant = Participant.create(user_id: current_user.id, event_id: @event.id)
     return unless @participant.save
 
-    respond_to do |format|
-      format.html { redirect_to user_event_path(@event), notice: 'Record successfully created.' }
-      format.turbo_stream
-    end
+    redirect_to user_event_path(@event), notice: 'Record successfully created.'
   end
 
   def destroy
     @participant = Participant.find_by(user_id: current_user.id, event_id: @event.id)
     if @participant
       @participant.destroy
-      respond_to do |format|
-        format.html { redirect_to user_event_path(@event), notice: 'Record successfully destroyed.' }
-        format.turbo_stream
-      end
+      redirect_to user_event_path(@event), notice: 'Record successfully destroyed.'
     else
-      respond_to do |format|
-        format.html { redirect_to user_event_path(@event), alert: 'Record not found.' }
-        format.turbo_stream
-      end
+      redirect_to user_event_path(@event), alert: 'Record not found.'
     end
   end
 

6908cd1fbdfb1a98cc1f76980e5db68ed6f670f7
Ranjan Nagarkoti <ranjan.nagarkoti@bajratechnologies.com>
2023-09-13 10:38:28 +0545

    feat: welcome mail sent to users after signup

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "llms@bajratechnologies.com"
   layout "mailer"
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,13 +1,10 @@
 class UserMailer < ApplicationMailer
-
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.user_mailer.welcome_message.subject
   #
-  def welcome_message
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def welcome_message(email)
+    mail to: email, subject: 'Welcome to the Lunch and Learn Management System!'
   end
 end

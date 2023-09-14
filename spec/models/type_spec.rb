5198bd4a77e92e5a927062d21273aa310cae0037
Ranjan Nagarkoti <ranjan.nagarkoti@bajratechnologies.com>
2023-09-14 23:40:15 +0545

    test: type model unit testing

--- spec/models/type_spec.rb
+++ spec/models/type_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Type, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    let(:type) { FactoryBot.build(:type, name: attribute_value) }
+
+    context 'with valid attributes' do
+      let(:attribute_value) { 'Valid Type Name' }
+
+      it 'is valid' do
+        expect(type).to be_valid
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:attribute_value) { nil }
+
+      it 'is invalid' do
+        expect(type).not_to be_valid
+      end
+
+      it 'raises validation error when saved' do
+        expect { type.save! }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+  end
+
+  context 'associations' do
+    it 'has many events' do
+      association = described_class.reflect_on_association(:events)
+      expect(association.macro).to eq(:has_many)
+    end
+  end
 end

81404eb98ae16695a3fb2163e1eee61a051f01a7
Ranjan Nagarkoti <ranjan.nagarkoti@bajratechnologies.com>
2023-09-15 07:43:34 +0545

    test: user model unit testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'john@gmail.com' }
+    password { 'Password1!' }
+    role { 0 }
+    username { 'JohnDoe' }
+    first_name { 'John' }
+    last_name { 'Doe' }
+    avatar { nil }
   end
 end
